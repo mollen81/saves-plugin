dbb0781acd5191dc8e7f90f1a82c7e0a65f43628
mollen <stepanpanenko@gmail.com>
2026-01-06 14:01:56 +0300

    Plugin.java extended with onDisable() logic

--- src/main/java/data_managers/PlayerDataManager.java
+++ src/main/java/data_managers/PlayerDataManager.java
@@ -1,27 +1,11 @@
 package data_managers;
 
-import org.bukkit.Effect;
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.potion.PotionEffect;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.UUID;
 
 public class PlayerDataManager
 {
     public static void savePlayerData(Player player)
     {
-        UUID uuid = player.getUniqueId();
-
-        Location location = player.getLocation();
-        Inventory inventory = player.getInventory();
-        double health = player.getHealth();
-        double experience = player.getExp();
-        Collection<PotionEffect> effects = player.getActivePotionEffects();
-
         player.saveData();
     }
 }
--- src/main/java/plugin/Plugin.java
+++ src/main/java/plugin/Plugin.java
@@ -1,6 +1,11 @@
 package plugin;
 
+import data_managers.PlayerDataManager;
+import data_managers.WorldDataManager;
 import listeners.PlayerQuitListener;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin
@@ -10,11 +15,22 @@ public class Plugin extends JavaPlugin
     {
         getServer().getPluginManager().registerEvents(
                 new PlayerQuitListener(this), this);
+
+        getLogger().info("Saves-plugin enabled");
     }
 
     @Override
-    public void onDisable()
-    {
+    public void onDisable() {
 
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            PlayerDataManager.savePlayerData(player);
+        }
+
+        for (World world : Bukkit.getWorlds())
+        {
+            WorldDataManager.saveWorldData(world);
+        }
+
+        getLogger().info("Saves-plugin disabled");
     }
 }
